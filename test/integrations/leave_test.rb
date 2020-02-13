28b997e4dfb71a4d2ec79692e25753f4a2748968
Delton Ding <dsh0416@gmail.com>
2020-02-14 07:04:31 +0900

    Update test

--- test/integrations/leave_test.rb
+++ test/integrations/leave_test.rb
@@ -11,14 +11,14 @@ class Petri::IntegrationLeaveTest < Minitest::Test
         t.produce :rejected, name: 'Rejected', with_guard: :rejected
       end
 
-      net.transition :hr_evaluate, consume: :leader_approved do |t|
-        t.produce :hr_approved, with_guard: :approved
+      net.transition :hr_evaluate, name: 'HR Evaluate', consume: :leader_approved do |t|
+        t.produce :hr_approved, name: 'HR Approved', with_guard: :approved
         t.produce :rejected, with_guard: :rejected
       end
 
-      net.transition :report_back, consume: :hr_approved, produce: :end
+      net.transition :report_back, name: 'Report Back', consume: :hr_approved, produce: :end
 
-      net.transition :resend_request, consume: :rejected do |t|
+      net.transition :resend_request, name: 'Resend Request', consume: :rejected do |t|
         t.produce :start, with_guard: :resend
         t.produce :end, with_guard: :discard
       end
@@ -28,5 +28,20 @@ class Petri::IntegrationLeaveTest < Minitest::Test
   end
 
   def test_compiled
+    assert_equal(@compiled, {
+      places: [
+        {label: :start, name: "Start"},
+        {label: :end, name: "End"},
+        {label: :leader_approved, name: "Leader Approved"},
+        {label: :rejected, name: "Rejected"},
+        {label: :hr_approved, name: "HR Approved"}
+      ],
+      transitions: [
+        {label: :leader_evaluate, name: "Leader Evaluate", consume: [:start], produce: [{label: :leader_approved, guard: :approved}, {label: :rejected, guard: :rejected}]},
+        {label: :hr_evaluate, name: "HR Evaluate", consume: [:leader_approved], produce: [{label: :hr_approved, guard: :approved}, {label: :rejected, guard: :rejected}]},
+        {label: :report_back, name: "Report Back", consume: [:hr_approved], produce: [{label: :end, guard: nil}]},
+        {label: :resend_request, name: "Resend Request", consume: [:rejected], produce: [{label: :start, guard: :resend}, {label: :end, guard: :discard}]}],
+      start_place: :start,
+      end_place: :end})
   end
 end
